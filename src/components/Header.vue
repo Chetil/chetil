05bdadfed14b59b370ab28c3c3988c587503ff78
KjetilCodes <post@kjetil.codes>
2021-03-10 21:26:10 +0100

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+// "Header
+<template>
+  <header>
+    <h1>./ Chetil</h1>
+  </header>
+</template>
+
+<script lang='ts'>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Header",
+});
+</script>
+
+<style  lang='scss'  scoped>
+header {
+  background: #223344;
+}
+</style>
+
